a45dcf51146214e312e258af2f9e32d8911ff8c9
Leonardo Aguiar Batista <85715692+leoaguiar995@users.noreply.github.com>
2025-04-16 17:04:05 -0300

    Exercicio 01
    v 1.1

---
+++ ex01/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', carregar)
+
+function carregar(){
+    var message = document.getElementById('message')
+    var image = document.getElementById('image')
+
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 2
+
+    message.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        image.src = './imgs/fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >= 12 && hora < 18){
+        image.src = './imgs/fototarde.png'
+        document.body.style.background = '#b9846f'
+    }else{
+        image.src = './imgs/fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
